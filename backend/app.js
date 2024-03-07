556d49350fc3a634c6e3ad90a2c1058b38adbdeb
Chaman Yadav <76009420+chaman56@users.noreply.github.com>
2024-03-07 23:08:18 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -19,7 +19,10 @@ dbConnect();
 const app = express();
 app.use(express.json())
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+  origin: ['https://aikart.vercel.app/'],
+  credentials: true
+}));
 app.use(express.static("aikart/backend/public"));
 
 app.use('/api/auth', authRouter);
